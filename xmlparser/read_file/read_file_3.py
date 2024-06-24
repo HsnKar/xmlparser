4dda5605d76a7000ce33461390d3f2469671e5ab
HsnKar <123533428+HsnKar@users.noreply.github.com>
2024-06-24 16:19:57 +0300

    Added the checking system to the file

---
+++ xmlparser/read_file/read_file_3.py
@@ -0,0 +1,37 @@
+import xml.etree.ElementTree as ET
+import csv
+import os
+
+path = "C:/Users/fkz_tmp/Documents/Projet Power BI/Local Tests/HO"
+dir_list = os.listdir(path)
+
+tree = ET.parse(
+    "C:/Users/fkz_tmp/Documents/Projet Power BI/Local Tests/HO/" + 'HOST02_pmresult_83888202_60_202406190000_202406190100.xml')
+root = tree.getroot()
+
+if os.path.exists('ho.csv'):
+    print("File already exists")
+else:
+    cols = ['start_time', 'MSC']
+
+    for field in root.findall('.//mt'):
+        cols.append(field.text)
+
+    with open('ho.csv', 'w', newline='', encoding="utf-8") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(cols)
+
+    # Loop through the directory
+    for file in dir_list:
+        tree = ET.parse("C:/Users/fkz_tmp/Documents/Projet Power BI/Local Tests/HO/" + file)
+        root = tree.getroot()
+        start_time = root.find('.//mts').text
+        # Added this date formatter
+        start = f"{start_time[:4]}-{start_time[4:6]}-{start_time[6:8]} {start_time[8:10]}:{start_time[11:]}0"
+        with open('ho.csv', 'a', newline='', encoding="utf-8") as csv_file:
+            writer = csv.writer(csv_file)
+            for record in root.findall('.//mv'):
+                row = []
+                for field in record:
+                    row.append(field.text)
+                writer.writerow([start] + row)
